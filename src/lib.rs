b89c3311271287d0c9653ffb6424e125a6b1137a
bonedaddy <boned4ddy@protonmail.com>
2022-04-01 20:47:49 -0700

    remove public static sighashdb

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,24 @@
 //! a variety of solana programs. see the `test` module below for an example of how
 //! to manually calculate sighashes
 
-use lazy_static::lazy_static;
-use std::collections::HashMap;
 
-lazy_static! {
-    /// a collection of sighashes with the `global:` prefix
-    pub static ref GLOBAL_SIGHASHDB: HashMap<&'static str, [u8; 8]> = {
-        let mut m = HashMap::new();
+pub struct GlobalSighashDB;
 
-        m.insert("deposit", [242, 35, 198, 137, 82, 225, 242, 182]);
-        m.insert("create_staker", [14, 28, 165, 74, 243, 144, 108, 177]);
-        m.insert("stake", [206, 176, 202, 18, 200, 209, 179, 108]);
-        m.insert("stake_dual_crop", [241, 42, 177, 56, 14, 203, 117, 253]);
-        m.insert("unstake", [90, 95, 107, 42, 205, 124, 50, 225]);
-        m.insert("unstake_dual_crop", [125, 31, 2, 239, 223, 165, 240, 249]);
-        m.insert("claim", [62, 198, 214, 193, 213, 159, 108, 210]);
-        m.insert("claim_dual_drop", [128, 32, 146, 208, 138, 252, 110, 71]);
-        m.insert("withdraw", [183, 18, 70, 156, 148, 109, 161, 34]);
-        m
-    };
+impl GlobalSighashDB {
+    pub fn get(&self, val: &str) -> Option<[u8; 8]> {
+        match val {
+            "deposit" => Some([242, 35, 198, 137, 82, 225, 242, 182]),
+            "create_staker" =>Some([14, 28, 165, 74, 243, 144, 108, 177]),
+            "stake" => Some([206, 176, 202, 18, 200, 209, 179, 108]),
+            "stake_dual_crop" => Some([241, 42, 177, 56, 14, 203, 117, 253]),
+            "unstake" => Some([90, 95, 107, 42, 205, 124, 50, 225]),
+            "unstake_dual_crop" => Some([125, 31, 2, 239, 223, 165, 240, 249]),
+            "claim" => Some([62, 198, 214, 193, 213, 159, 108, 210]),
+            "claim_dual_crop" => Some([128, 32, 146, 208, 138, 252, 110, 71]),
+            "withdraw" => Some([183, 18, 70, 156, 148, 109, 161, 34]),
+            _ => None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -38,14 +37,14 @@ mod test {
     }
     #[test]
     fn test_global_sighashdb() {
-        assert_eq!(*GLOBAL_SIGHASHDB.get("deposit").unwrap(), [242, 35, 198, 137, 82, 225, 242, 182]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("create_staker").unwrap(), [14, 28, 165, 74, 243, 144, 108, 177]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("stake").unwrap(), [206, 176, 202, 18, 200, 209, 179, 108]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("stake_dual_crop").unwrap(), [241, 42, 177, 56, 14, 203, 117, 253]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("unstake").unwrap(), [90, 95, 107, 42, 205, 124, 50, 225]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("unstake_dual_crop").unwrap(), [125, 31, 2, 239, 223, 165, 240, 249]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("claim").unwrap(), [62, 198, 214, 193, 213, 159, 108, 210]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("claim_dual_drop").unwrap(), [128, 32, 146, 208, 138, 252, 110, 71]);
-        assert_eq!(*GLOBAL_SIGHASHDB.get("withdraw").unwrap(), [183, 18, 70, 156, 148, 109, 161, 34]);
+        assert_eq!(GlobalSighashDB.get("deposit").unwrap(), [242, 35, 198, 137, 82, 225, 242, 182]);
+        assert_eq!(GlobalSighashDB.get("create_staker").unwrap(), [14, 28, 165, 74, 243, 144, 108, 177]);
+        assert_eq!(GlobalSighashDB.get("stake").unwrap(), [206, 176, 202, 18, 200, 209, 179, 108]);
+        assert_eq!(GlobalSighashDB.get("stake_dual_crop").unwrap(), [241, 42, 177, 56, 14, 203, 117, 253]);
+        assert_eq!(GlobalSighashDB.get("unstake").unwrap(), [90, 95, 107, 42, 205, 124, 50, 225]);
+        assert_eq!(GlobalSighashDB.get("unstake_dual_crop").unwrap(), [125, 31, 2, 239, 223, 165, 240, 249]);
+        assert_eq!(GlobalSighashDB.get("claim").unwrap(), [62, 198, 214, 193, 213, 159, 108, 210]);
+        assert_eq!(GlobalSighashDB.get("claim_dual_crop").unwrap(), [128, 32, 146, 208, 138, 252, 110, 71]);
+        assert_eq!(GlobalSighashDB.get("withdraw").unwrap(), [183, 18, 70, 156, 148, 109, 161, 34]);
     }
 }
